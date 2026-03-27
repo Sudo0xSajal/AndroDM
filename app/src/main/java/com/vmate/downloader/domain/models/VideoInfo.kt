f77f22d7e56c6c8b80b36d363c0083d257b77446
Sajal Haldar <sajalhaldar2004@gmail.com>
2026-03-28 04:14:18 +0530

    Create VideoInfo data class

---
+++ app/src/main/java/com/vmate/downloader/domain/models/VideoInfo.kt
@@ -0,0 +1,17 @@
+data class VideoInfo(
+    val id: String,
+    val url: String,
+    val title: String,
+    val thumbnailUrl: String,
+    val description: String,
+    val channel: String,
+    val uploaderUrl: String,
+    val durationSeconds: Int,
+    val viewCount: Int,
+    val likeCount: Int,
+    val uploadDate: String,
+    val formats: List<String>,
+    val isPlaylist: Boolean,
+    val playlistTitle: String?,
+    val playlistCount: Int?
+)
