531618a56483116e38fd5f8200f05a044c4f9a9a
Sajal Haldar <sajalhaldar2004@gmail.com>
2026-03-28 04:28:05 +0530

    Updated FormatInfo.kt with package declaration on 2026-03-27 22:57:38

---
+++ FormatInfo.kt
@@ -0,0 +1,18 @@
+package com.vmate.downloader.domain.models
+
+data class FormatInfo(
+    val formatId: String,
+    val ext: String,
+    val resolution: String,
+    val fps: Int,
+    val filesize: Int,
+    val tbr: Int,
+    val vbr: Int,
+    val abr: Int,
+    val acodec: String,
+    val vcodec: String,
+    val quality: String,
+    val isAudioOnly: Boolean,
+    val isVideoOnly: Boolean,
+    val url: String
+)
